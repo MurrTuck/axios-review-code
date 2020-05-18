bc7cab78d5c8aee0948fdfb525491df7a6918639
MurrTuck <murrtuck@gmail.com>
2020-05-18 10:16:05 -0600

    Morning Review Done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
+import axios from 'axios';
 import Form from './Components/Form';
 import VehicleDisplay from './Components/VehicleDisplay';
 import './App.css';
 
-class App extends Component  {
-  constructor(props){
+class App extends Component {
+  constructor(props) {
     super(props);
     this.state = {
       vehicles: []
     }
+    this.addVehicle = this.addVehicle.bind(this);
   }
 
-  render(){
+  componentDidMount() {
+    axios.get("https://joes-autos.herokuapp.com/api/vehicles")
+      .then(res => {
+        this.setState({ vehicles: res.data })
+      })
+      .catch(err => console.log(err));
+  }
+
+  addVehicle(newCar) {
+    axios.post("https://joes-autos.herokuapp.com/api/vehicles", newCar)
+      .then(res => {
+        this.setState({ vehicles: res.data.vehicles });
+      })
+      .catch(err => console.log(err))
+  }
+
+  render() {
     console.log(this.state.vehicles)
     const mappedVehicles = this.state.vehicles.map((vehicle, i) => (
-      <VehicleDisplay key={i} vehicle={vehicle}/>
+      <VehicleDisplay key={i} vehicle={vehicle} />
     ))
     return (
       <div className="App">
         WR1 HTTP/Axios Review
-        <Form />
+        <Form addVehicle={this.addVehicle} />
         {mappedVehicles}
       </div>
     )
--- src/Components/Form.js
+++ src/Components/Form.js
@@ -1,7 +1,7 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 
 export default class Form extends Component {
-    constructor(props){
+    constructor(props) {
         super(props);
         this.state = {
             make: '',
@@ -13,18 +13,24 @@ export default class Form extends Component {
     }
 
     handleChange = (event) => {
-        this.setState({[event.target.name]: event.target.value})
+        this.setState({ [event.target.name]: event.target.value })
     }
 
-    render(){
-        const {make, model, year, color, price} = this.state
-        return(
+    addNewCar = () => {
+        const { make, model, year, color, price } = this.state;
+        const newCar = { make, model, year, color, price };
+        this.props.addVehicle(newCar);
+    }
+
+    render() {
+        const { make, model, year, color, price } = this.state
+        return (
             <div>
-                <input name='make' value={make} onChange={e => this.handleChange(e)} placeholder='make'/>
-                <input name='model' value={model} onChange={e => this.handleChange(e)} placeholder='model'/>
-                <input name='year' value={year} onChange={e => this.handleChange(e)} placeholder='year' type='number'/>
-                <input name='color' value={color} onChange={e => this.handleChange(e)} placeholder='color'/>
-                <input name='price' value={price} onChange={e => this.handleChange(e)} placeholder='price' type='number'/>
+                <input name='make' value={make} onChange={e => this.handleChange(e)} placeholder='make' />
+                <input name='model' value={model} onChange={e => this.handleChange(e)} placeholder='model' />
+                <input name='year' value={year} onChange={e => this.handleChange(e)} placeholder='year' type='number' />
+                <input name='color' value={color} onChange={e => this.handleChange(e)} placeholder='color' />
+                <input name='price' value={price} onChange={e => this.handleChange(e)} placeholder='price' type='number' />
                 <button onClick={this.addNewCar}>Add Vehicle</button>
             </div>
         )
